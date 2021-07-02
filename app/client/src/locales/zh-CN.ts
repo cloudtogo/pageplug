3881c71b20305a26b88d18c2b3c05b12b9d2528d
王昆 <quincy@cloudtogo.cn>
2021-07-02 17:56:57 +0800

    [+] add antd, i18n

---
+++ app/client/src/locales/zh-CN.ts
@@ -0,0 +1,11 @@
+import header from "./zh-CN/header";
+import applications from "./zh-CN/applications";
+
+export default {
+  "navBar.lang": "语言",
+  "layout.user.link.help": "帮助",
+  "layout.user.link.privacy": "隐私",
+  "layout.user.link.terms": "条款",
+  ...header,
+  ...applications,
+};
---
+++ app/client/src/locales/zh-CN/applications.ts
@@ -0,0 +1,5 @@
+export default {
+  "applications.menu.organization": "应用组",
+  "applications.menu.organization.create": "创建分组",
+  "applications.menu.start": "功能指南",
+};
