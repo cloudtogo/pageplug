8d6829374618bf296069066a7bf7d5ddc404a45f
王昆 <quincy@cloudtogo.cn>
2021-07-02 17:56:57 +0800

    [+] add antd, i18n

---
+++ app/client/src/locales/zh-CN/header.ts
@@ -0,0 +1,7 @@
+export default {
+  "header.user.editProfile": "修改个人资料",
+  "header.user.signOut": "退出登录",
+  "header.action.share": "分享应用",
+  "header.action.deploy": "部署应用",
+  "header.action.current": "当前版本",
+};
