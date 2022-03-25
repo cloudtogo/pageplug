120436d09950d8ae865e92de02c0dae9ecd5fe7f
王昆 <quincy@cloudtogo.cn>
2022-03-25 19:44:19 +0800

    [!] form bug fixed

--- app/client/src/components/designSystems/taro/FormComponent/AddressField.tsx
+++ app/client/src/components/designSystems/taro/FormComponent/AddressField.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ArrowRight } from "@taroify/icons";
 import { Form, AreaPicker, Input } from "@taroify/core";
 import { FormItemInstance } from "@taroify/core/form";
@@ -12,22 +12,31 @@ export interface FieldProps {
   placeholder?: string;
 }
 
+const provinceList: any = areaList.province_list;
+const cityList: any = areaList.city_list;
+const countyList: any = areaList.county_list;
+
+const hiddenStyle = {
+  height: 0,
+  padding: 0,
+};
+
 function PickerField({ label, name, required, placeholder }: FieldProps) {
   const itemRef = useRef<FormItemInstance>();
+  const provinceNameRef = useRef<FormItemInstance>();
+  const cityNameRef = useRef<FormItemInstance>();
+  const countyNameRef = useRef<FormItemInstance>();
   const [open, setOpen] = useState(false);
-
+  const [areaValue, setAreaValue] = useState<string[]>();
   const current = itemRef.current?.getValue();
-  let currentLabel: any = undefined;
-  if (current?.length === 3) {
-    const provinceList: any = areaList.province_list;
-    const cityList: any = areaList.city_list;
-    const countryList: any = areaList.county_list;
-    currentLabel = [
-      provinceList[current[0]],
-      cityList[current[1]],
-      countryList[current[2]],
-    ].join("");
-  }
+
+  useEffect(() => {
+    if (current) {
+      const currentProvince = current.substr(0, 2) + "0000";
+      const currentCity = current.substr(0, 4) + "00";
+      setAreaValue([currentProvince, currentCity, current]);
+    }
+  }, [current]);
 
   return (
     <>
@@ -40,13 +49,37 @@ function PickerField({ label, name, required, placeholder }: FieldProps) {
       >
         <Form.Label>{label}</Form.Label>
         <Form.Control>
-          <Input
-            readonly
-            placeholder={placeholder || `请选择${label}`}
-            onClick={() => setOpen(true)}
-            value={currentLabel}
-          />
+          {(props) => {
+            let currentLabel = "";
+            const county = props.value;
+            if (county?.length === 6) {
+              const province = county.substr(0, 2) + "0000";
+              const city = county.substr(0, 4) + "00";
+              currentLabel = [
+                provinceList[province],
+                cityList[city],
+                countyList[county],
+              ].join("");
+            }
+            return (
+              <Input
+                readonly
+                placeholder={placeholder || `请选择${label}`}
+                onClick={() => setOpen(true)}
+                value={currentLabel}
+              />
+            );
+          }}
         </Form.Control>
+      </Form.Item>
+      <Form.Item name="province" ref={provinceNameRef} style={hiddenStyle}>
+        <Input readonly />
+      </Form.Item>
+      <Form.Item name="city" ref={cityNameRef} style={hiddenStyle}>
+        <Input readonly />
+      </Form.Item>
+      <Form.Item name="county" ref={countyNameRef} style={hiddenStyle}>
+        <Input readonly />
       </Form.Item>
       <PortalPopup
         mountOnEnter={false}
@@ -56,9 +89,13 @@ function PickerField({ label, name, required, placeholder }: FieldProps) {
         onClose={setOpen}
       >
         <AreaPicker
+          value={areaValue}
           onCancel={() => setOpen(false)}
           onConfirm={(newValue) => {
-            itemRef.current?.setValue(newValue);
+            itemRef.current?.setValue(newValue[2]);
+            provinceNameRef.current?.setValue(provinceList[newValue[0]]);
+            cityNameRef.current?.setValue(cityList[newValue[1]]);
+            countyNameRef.current?.setValue(countyList[newValue[2]]);
             setOpen(false);
           }}
         >
