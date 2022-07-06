ca1b949312b8eef344be421523ccad7bf9f49dc4
王昆 <quincy@cloudtogo.cn>
2022-07-06 14:27:18 +0800

    [!] merge bug fix

--- app/client/src/components/designSystems/taro/ActionBar/ActionBarContext.ts
+++
@@ -1,9 +0,0 @@
-import { createContext, Context } from "react";
-
-export interface ActionBarState {
-  parent?: Record<string, any>;
-}
-
-const ActionButtonContext: Context<ActionBarState> = createContext({});
-
-export default ActionButtonContext;
