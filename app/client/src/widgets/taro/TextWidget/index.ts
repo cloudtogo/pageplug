7d68fe0c42989d36b494df1ce4b2a5b7e5fcabca
王昆 <quincy@cloudtogo.cn>
2022-07-06 11:59:19 +0800

    [!] merge bug fix

---
+++ app/client/src/widgets/taro/TextWidget/index.ts
@@ -0,0 +1,29 @@
+import Widget from "./widget";
+import IconSVG from "./icon.svg";
+
+export const CONFIG = {
+  type: Widget.getWidgetType(),
+  name: "文本",
+  iconSVG: IconSVG,
+  needsMeta: false,
+  isCanvas: false,
+  isMobile: true,
+  defaults: {
+    widgetName: "text",
+    text: "文本",
+    fontSize: "PARAGRAPH",
+    textAlign: "LEFT",
+    textColor: "#333",
+    rows: 4,
+    columns: 16,
+    version: 1,
+  },
+  properties: {
+    derived: Widget.getDerivedPropertiesMap(),
+    default: Widget.getDefaultPropertiesMap(),
+    meta: Widget.getMetaPropertiesMap(),
+    config: Widget.getPropertyPaneConfig(),
+  },
+};
+
+export default Widget;
