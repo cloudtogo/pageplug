ddaeadd833701e2165908a0320f37155fc661725
王昆 <quincy@cloudtogo.cn>
2022-08-29 21:57:00 +0800

    merge bug fix

--- app/client/src/widgets/MultiSelectWidget/widget/index.tsx
+++ app/client/src/widgets/MultiSelectWidget/widget/index.tsx
@@ -273,7 +273,7 @@ class MultiSelectWidget extends BaseWidget<
           },
           {
             propertyName: "labelTextColor",
-            label: "Label Text Color",
+            label: "标签文本颜色",
             controlType: "COLOR_PICKER",
             isJSConvertible: true,
             isBindProperty: true,
