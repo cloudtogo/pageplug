31ee3a35c6c4684715a7fbac861a2843e97f6988
王昆 <quincy@cloudtogo.cn>
2022-08-29 17:26:41 +0800

    merge bug fix

--- app/client/src/widgets/TableWidgetV2/widget/propertyConfig/PanelConfig/MenuItems.ts
+++ app/client/src/widgets/TableWidgetV2/widget/propertyConfig/PanelConfig/MenuItems.ts
@@ -118,7 +118,7 @@ export default {
             ],
           },
           {
-            sectionName: "Icon Options",
+            sectionName: "图标配置",
             children: [
               {
                 propertyName: "iconName",
