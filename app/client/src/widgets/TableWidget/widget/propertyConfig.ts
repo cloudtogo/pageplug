cd4df9491b204d16dfa9dde68c35e7f1ff3ae959
王昆 <quincy@cloudtogo.cn>
2022-08-30 14:41:11 +0800

    merge bug fix

--- app/client/src/widgets/TableWidget/widget/propertyConfig.ts
+++ app/client/src/widgets/TableWidget/widget/propertyConfig.ts
@@ -96,7 +96,7 @@ export default [
                       value: "url",
                     },
                     {
-                      label: "Number",
+                      label: "数字",
                       value: "number",
                     },
                     {
