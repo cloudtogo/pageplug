35db2651e64bde65f86aba5cca02e39989a4dcdb
王昆 <quincy@cloudtogo.cn>
2022-08-30 10:27:30 +0800

    merge bug fix

--- app/client/src/widgets/InputWidgetV2/widget/index.tsx
+++ app/client/src/widgets/InputWidgetV2/widget/index.tsx
@@ -224,7 +224,7 @@ class InputWidget extends BaseInputWidget<InputWidgetProps, WidgetState> {
             },
             {
               helpText:
-                "Sets the default text of the widget. The text is updated if the default text changes",
+                "设置组件默认值，当默认值改变后，组件当前值会自动更新",
               propertyName: "defaultText",
               label: "默认值",
               controlType: "INPUT_TEXT",
@@ -374,7 +374,7 @@ class InputWidget extends BaseInputWidget<InputWidgetProps, WidgetState> {
             },
             {
               helpText:
-                "Sets the default text of the widget. The text is updated if the default text changes",
+                "设置组件默认值，当默认值改变后，组件当前值会自动更新",
               propertyName: "defaultText",
               label: "Default Value",
               controlType: "INPUT_TEXT",
