92fb71497f9a8aba3eb638e8814706ea3c4170f3
王昆 <quincy@cloudtogo.cn>
2022-08-30 10:37:37 +0800

    merge bug fix

--- app/client/src/widgets/DatePickerWidget2/widget/index.tsx
+++ app/client/src/widgets/DatePickerWidget2/widget/index.tsx
@@ -398,7 +398,7 @@ class DatePickerWidget extends BaseWidget<DatePickerWidget2Props, WidgetState> {
           {
             helpText: "Sets the format of the selected date",
             propertyName: "dateFormat",
-            label: "Date Format",
+            label: "日期格式",
             controlType: "DROP_DOWN",
             isJSConvertible: true,
             optionWidth: "340px",
