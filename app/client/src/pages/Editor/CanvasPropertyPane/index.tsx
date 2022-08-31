20b9f5f83fec4718d991a22c5f06ec214dc7f3ec
王昆 <quincy@cloudtogo.cn>
2022-08-31 17:15:36 +0800

    merge bug fix

--- app/client/src/pages/Editor/CanvasPropertyPane/index.tsx
+++ app/client/src/pages/Editor/CanvasPropertyPane/index.tsx
@@ -11,11 +11,11 @@ type Props = {
 export function CanvasPropertyPane(props: Props) {
   return (
     <div className="relative ">
-      <h3 className="px-3 py-3 text-sm font-medium uppercase">Properties</h3>
+      <h3 className="px-3 py-3 text-sm font-medium uppercase">画布属性</h3>
 
       <div className="mt-3 space-y-6">
         <div className="px-3 space-y-2">
-          <p className="text-sm text-gray-700">Canvas Size</p>
+          <p className="text-sm text-gray-700">画布尺寸</p>
           <MainContainerLayoutControl />
         </div>
 
