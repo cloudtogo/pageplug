02aa33fa97bce38091662f5a65fe940719c514d2
王昆 <quincy@cloudtogo.cn>
2022-09-22 19:37:33 +0800

    [!] merge bug fix

--- cloudos_deploy/start-nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script is used to boot Nginx when the Docker container starts
-set -ue
-cat /nginx.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^PAGEPLUG_[A-Z0-9_]+'))" | sed -e 's|\${\(PAGEPLUG_[A-Z0-9_]*\)}||g' > /etc/nginx/conf.d/default.conf
-cat /nginx-root.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^PAGEPLUG_[A-Z0-9_]+'))" | sed -e 's|\${\(PAGEPLUG_[A-Z0-9_]*\)}||g' > /etc/nginx/nginx.conf
-exec nginx -g 'daemon off;'
