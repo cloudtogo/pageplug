13e0c77ad827984900a2828eb07e613a54f561ac
王昆 <quincy@cloudtogo.cn>
2022-11-07 17:40:02 +0800

    [!] merge fix

--- app/client/src/widgets/RadioGroupWidget/widget/index.tsx
+++ app/client/src/widgets/RadioGroupWidget/widget/index.tsx
@@ -358,7 +358,7 @@ class RadioGroupWidget extends BaseWidget<RadioGroupWidgetProps, WidgetState> {
           {
             propertyName: "labelTextColor",
             label: "字体颜色",
-            helpText: "Control the color of the label associated",
+            helpText: "设置标签字体颜色",
             controlType: "COLOR_PICKER",
             isJSConvertible: true,
             isBindProperty: true,
@@ -368,7 +368,7 @@ class RadioGroupWidget extends BaseWidget<RadioGroupWidgetProps, WidgetState> {
           {
             propertyName: "labelTextSize",
             label: "字体大小",
-            helpText: "Control the font size of the label associated",
+            helpText: "设置标签字体大小",
             controlType: "DROP_DOWN",
             defaultValue: "0.875rem",
             options: [
@@ -411,7 +411,7 @@ class RadioGroupWidget extends BaseWidget<RadioGroupWidgetProps, WidgetState> {
           {
             propertyName: "labelStyle",
             label: "强调",
-            helpText: "Control if the label should be bold or italics",
+            helpText: "设置标签字体是否加粗或斜体",
             controlType: "BUTTON_TABS",
             options: [
               {
--- app/client/src/widgets/SwitchGroupWidget/widget/index.tsx
+++ app/client/src/widgets/SwitchGroupWidget/widget/index.tsx
@@ -240,7 +240,7 @@ class SwitchGroupWidget extends BaseWidget<
           {
             propertyName: "labelTextColor",
             label: "字体颜色",
-            helpText: "Control the color of the label associated",
+            helpText: "设置标签字体颜色",
             controlType: "COLOR_PICKER",
             isJSConvertible: true,
             isBindProperty: true,
@@ -250,7 +250,7 @@ class SwitchGroupWidget extends BaseWidget<
           {
             propertyName: "labelTextSize",
             label: "字体大小",
-            helpText: "Control the font size of the label associated",
+            helpText: "设置标签字体大小",
             controlType: "DROP_DOWN",
             defaultValue: "0.875rem",
             options: [
@@ -293,7 +293,7 @@ class SwitchGroupWidget extends BaseWidget<
           {
             propertyName: "labelStyle",
             label: "强调",
-            helpText: "Control if the label should be bold or italics",
+            helpText: "设置标签字体是否加粗或斜体",
             controlType: "BUTTON_TABS",
             options: [
               {
