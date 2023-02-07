91c4780676651d2da77969cb7bdce8c252e9c8b9
王昆 <quincy@cloudtogo.cn>
2023-02-07 10:32:25 +0800

    [!] windows install bug

--- app/shared/cross.js
+++ app/shared/cross.js
@@ -1,6 +1,6 @@
 const crossify = (command = '') => {
   if (process.platform === 'win32') {
-    return command.trim().replace(/;/g, '').replace(/\n/g, '&&');
+    return command.trim().replace(/;/g, '').replace(/\n/g, '&&').replace(/\//g, '\\');
   }
   return command;
 }
