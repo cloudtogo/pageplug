cloudtogo/pageplug

commit 4b27decf0cf6208b8d6949d22e059d4d86fd40fc

feat: Add support for connecting to Redis clusters (#24924)
This PR adds support to Appsmith server to be able to connect to
ElastiCache Redis with cluster mode turned on. When the
`APPSMITH_REDIS_URL` is set to `redis://...`, the current default
behaviour is preserved. But if it is set to `redis-cluster://...`, then
we setup a pooled connection with cluster mod enabled.

app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/RedisConfig.java
@@ -8,10 +8,17 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.lettuce.core.resource.ClientResources;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
@@ -28,15 +35,20 @@ import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Configuration
 @Slf4j
 // Setting the maxInactiveInterval to 30 days
 @EnableRedisWebSession(maxInactiveIntervalInSeconds = 2592000)
 public class RedisConfig {
+
+    @Value("${appsmith.redis.url:}")
+    private String redisURL;
 
     /**
      * This is the topic to which we will publish & subscribe to. We can have multiple topics based on the messages
@@ -47,6 +59,31 @@ public class RedisConfig {
     @Bean
     ChannelTopic topic() {
         return new ChannelTopic("appsmith:queue");
+    }
+
+    @Bean
+    @Primary
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        final URI redisUri = URI.create(redisURL);
+        final String scheme = redisUri.getScheme();
+
+        switch (scheme) {
+            case "redis" -> {
+                return new LettuceConnectionFactory(redisUri.getHost(), redisUri.getPort());
+            }
+
+            case "redis-cluster" -> {
+                // For ElastiCache Redis with cluster mode enabled, with the configuration endpoint.
+                final LettuceClientConfiguration config = LettucePoolingClientConfiguration
+                    .builder()
+                    .build();
+                final RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+                clusterConfig.addClusterNode(new RedisNode(redisUri.getHost(), redisUri.getPort()));
+                return new LettuceConnectionFactory(clusterConfig, config);
+            }
+
+            default -> throw new InvalidRedisURIException("Invalid redis scheme: " + scheme);
+        }
     }
 
     @Bean
@@ -176,6 +213,12 @@ public class RedisConfig {
             }
 
             return fallback.deserialize(bytes);
+        }
+    }
+
+    private static class InvalidRedisURIException extends RuntimeException {
+        public InvalidRedisURIException(String message) {
+            super(message);
         }
     }
 

app/server/appsmith-server/pom.xml
@@ -224,6 +224,14 @@
             <scope>test</scope>
         </dependency>
 
+        <!-- Needed for establishing pooled connections to ElastiCache Redis with cluster mode enabled. -->
+        <dependency>
+            <groupId>org.apache.commons</groupId>
+            <artifactId>commons-pool2</artifactId>
+            <version>2.11.1</version>
+            <scope>runtime</scope>
+        </dependency>
+
         <dependency>
             <groupId>org.junit.platform</groupId>
             <artifactId>junit-platform-suite-engine</artifactId>

app/server/appsmith-server/src/main/resources/application.properties
@@ -54,7 +54,7 @@ sentry.debug=false
 sentry.environment=${APPSMITH_SERVER_SENTRY_ENVIRONMENT:}
 
 # Redis Properties
-spring.data.redis.url=${APPSMITH_REDIS_URL}
+appsmith.redis.url=${APPSMITH_REDIS_URL}
 
 # Mail Properties
 # Email defaults to false, because, when true and the other SMTP properties are not set, Spring will try to use a

commit 4f8eb51d859100bf391665193b95f4d301a1f9f1

fix: Fix Redis connections that require authentication (#25828)
This fixes a problem with connecting to Redis that require
authentication.
This is a regression introduced in #24924.

Fixes #25259

app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/RedisConfig.java
@@ -8,14 +8,17 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import io.lettuce.core.resource.ClientResources;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisNode;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
@@ -67,19 +70,32 @@ public class RedisConfig {
 
         switch (scheme) {
             case "redis" -> {
-                return new LettuceConnectionFactory(redisUri.getHost(), redisUri.getPort());
+                final RedisStandaloneConfiguration config =
+                        new RedisStandaloneConfiguration(redisUri.getHost(), redisUri.getPort());
+                fillAuthentication(redisUri, config);
+                return new LettuceConnectionFactory(config);
             }
 
             case "redis-cluster" -> {
                 // For ElastiCache Redis with cluster mode enabled, with the configuration endpoint.
-                final LettuceClientConfiguration config =
-                        LettucePoolingClientConfiguration.builder().build();
                 final RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
+                fillAuthentication(redisUri, clusterConfig);
                 clusterConfig.addClusterNode(new RedisNode(redisUri.getHost(), redisUri.getPort()));
-                return new LettuceConnectionFactory(clusterConfig, config);
+                return new LettuceConnectionFactory(
+                        clusterConfig,
+                        LettucePoolingClientConfiguration.builder().build());
             }
 
             default -> throw new InvalidRedisURIException("Invalid redis scheme: " + scheme);
+        }
+    }
+
+    private void fillAuthentication(URI redisUri, RedisConfiguration.WithAuthentication config) {
+        final String userInfo = redisUri.getUserInfo();
+        if (StringUtils.isNotEmpty(userInfo)) {
+            final String[] parts = userInfo.split(":", 2);
+            config.setUsername(parts[0]);
+            config.setPassword(RedisPassword.of(parts.length > 1 ? parts[1] : null));
         }
     }
 