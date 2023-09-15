17eda178e8494992c20f77d1194938610c5c3cb3
xingyao.liu <xingyao.liu@cloudtogo.cn>
2023-09-15 14:27:17 +0800

    feat: Merge DM & TiDB Plugin

---
+++ app/server/appsmith-plugins/tidbPlugin/src/main/java/com/external/plugins/utils/TidbErrorUtils.java
@@ -0,0 +1,58 @@
+package com.external.plugins.utils;
+
+import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginException;
+import com.appsmith.external.plugins.AppsmithPluginErrorUtils;
+import io.r2dbc.spi.R2dbcNonTransientResourceException;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TidbErrorUtils extends AppsmithPluginErrorUtils {
+
+    private static TidbErrorUtils tidbErrorUtils;
+
+    public static TidbErrorUtils getInstance() {
+        if (tidbErrorUtils == null) {
+            tidbErrorUtils = new TidbErrorUtils();
+        }
+        return tidbErrorUtils;
+    }
+
+    /**
+     * Extract small readable portion of error message from a larger less comprehensible error message.
+     * @param error - any error object
+     * @return readable error message
+     */
+    @Override
+    public String getReadableError(Throwable error) {
+        Throwable externalError;
+
+        // If the external error is wrapped inside Appsmith error, then extract the external error first.
+        if (error instanceof AppsmithPluginException) {
+            if (((AppsmithPluginException) error).getExternalError() == null) {
+                return error.getMessage();
+            }
+
+            externalError = ((AppsmithPluginException) error).getExternalError();
+        } else {
+            externalError = error;
+        }
+
+        if (externalError instanceof io.r2dbc.spi.R2dbcNonTransientResourceException) {
+
+            /**
+             *@param [9000] [H1000] Fail to establish connection to [host:port] :
+                                            Access denied for user 'username'@'host' (using password: NO)
+             *@return Access denied for user 'username'@'host'
+             */
+
+            R2dbcNonTransientResourceException r2dbcNonTransientResourceException = (R2dbcNonTransientResourceException) externalError;
+            return r2dbcNonTransientResourceException.getMessage().split(" : ")[1].
+                    split(" \\(")[0].trim();
+
+        }
+
+        return externalError.getMessage();
+    }
+}
+
