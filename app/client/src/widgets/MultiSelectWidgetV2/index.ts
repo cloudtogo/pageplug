529606fea41f26bf1dc75ab0e0c14f421e66ce83
zhaisifeng <zhaisifeng@cloudtogo.cn>
2023-10-24 17:22:02 +0800

    chore: re-select version change to 14.4.3

--- app/client/src/widgets/MultiSelectWidgetV2/index.ts
+++ app/client/src/widgets/MultiSelectWidgetV2/index.ts
@@ -25,7 +25,7 @@ export const CONFIG = {
   iconSVG: IconSVG,
   tags: [WIDGET_TAGS.SELECT],
   needsMeta: true,
-  searchTags: ["dropdown", "tags", "multi select"],
+  searchTags: ["dropdown", "tags"],
   defaults: {
     rows: 7,
     columns: 20,
