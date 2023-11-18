57b6a2c920049fc47e6ad6b189ea88e4696d0177
dengll <denglianglu@cloudtogo.cn>
2023-11-18 14:24:09 +0800

    fix: hide autocomplete switch

--- app/server/appsmith-server/src/main/java/com/appsmith/server/exceptions/AppsmithError.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/exceptions/AppsmithError.java
@@ -379,8 +379,7 @@ public enum AppsmithError {
     INVALID_GIT_REPO(
             400,
             AppsmithErrorCode.INVALID_GIT_REPO.getCode(),
-            "远程存储库不是空的。请创建一个新的空存储库并配置 SSH 密钥。 "
-                    + "如果您希望从现有的远程存储库克隆和构建应用程序，请在主页中使用 \"从 Git 存储库导入\" 选项。",
+            "远程存储库不是空的。请创建一个新的空存储库并配置 SSH 密钥。 " + "如果您希望从现有的远程存储库克隆和构建应用程序，请在主页中使用 \"从 Git 存储库导入\" 选项。",
             AppsmithErrorAction.DEFAULT,
             "Invalid Git repository",
             ErrorType.GIT_CONFIGURATION_ERROR,
