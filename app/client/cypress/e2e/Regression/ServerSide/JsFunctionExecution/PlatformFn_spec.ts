cloudtogo/pageplug

commit 74b8e7fa2c3dc75660db928f1acc7e17305799b5

fix: reset widget to default  value after setter method  (#29151)
## Description
This PR ensures that widgets are reset to their default value after the
`setValue` setter method is used to set its value

#### PR fixes following issue(s)
Fixes #27119 

#### Media
> A video or a GIF is preferred. when using Loom, don’t embed because it
looks like it’s a GIF. instead, just link to the video
>
>
#### Type of change
> Please delete options that are not relevant.
- Bug fix (non-breaking change which fixes an issue)
- New feature (non-breaking change which adds functionality)
- Breaking change (fix or feature that would cause existing
functionality to not work as expected)
- Chore (housekeeping or task changes that don't impact user perception)
- This change requires a documentation update
>
>
>
## Testing
>
#### How Has This Been Tested?
> Please describe the tests that you ran to verify your changes. Also
list any relevant details for your test configuration.
> Delete anything that is not relevant
- [ ] Manual
- [ ] JUnit
- [ ] Jest
- [ ] Cypress
>
>
#### Test Plan
> Add Testsmith test cases links that relate to this PR
>
>
#### Issues raised during DP testing
> Link issues raised during DP testing for better visiblity and tracking
(copy link from comments dropped on this PR)
>
>
>
## Checklist:
#### Dev activity
- [ ] My code follows the style guidelines of this project
- [ ] I have performed a self-review of my own code
- [ ] I have commented my code, particularly in hard-to-understand areas
- [ ] I have made corresponding changes to the documentation
- [ ] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [ ] New and existing unit tests pass locally with my changes
- [ ] PR is being merged under a feature flag


#### QA activity:
- [ ] [Speedbreak

app/client/src/sagas/EvaluationsSaga.ts
@@ -22,7 +22,7 @@ import {
   getDataTree,
   getUnevaluatedDataTree,
 } from "selectors/dataTreeSelectors";
-import { getMetaWidgets, getWidgets } from "sagas/selectors";
+import { getMetaWidgets, getWidgets, getWidgetsMeta } from "sagas/selectors";
 import type { WidgetTypeConfigMap } from "WidgetProvider/factory";
 import WidgetFactory from "WidgetProvider/factory";
 import { GracefulWorkerService } from "utils/WorkerUtil";
@@ -260,6 +260,8 @@ export function* evaluateTreeSaga(
     PerformanceTransactionName.DATA_TREE_EVALUATION,
   );
   const appMode: ReturnType<typeof getAppMode> = yield select(getAppMode);
+  const widgetsMeta: ReturnType<typeof getWidgetsMeta> =
+    yield select(getWidgetsMeta);
 
   const evalTreeRequestData: EvalTreeRequestData = {
     unevalTree: unEvalAndConfigTree,
@@ -271,6 +273,7 @@ export function* evaluateTreeSaga(
     forceEvaluation,
     metaWidgets,
     appMode,
+    widgetsMeta,
   };
 
   const workerResponse: EvalTreeResponseData = yield call(

app/client/src/workers/Evaluation/fns/resetWidget.ts
@@ -1,22 +1,284 @@
 import { promisify } from "./utils/Promisify";
 
-function resetWidgetFnDescriptor(widgetName: string, resetChildren = true) {
+import type { FlattenedWidgetProps } from "WidgetProvider/constants";
+import {
+  canvasWidgets,
+  dataTreeEvaluator,
+  canvasWidgetsMeta,
+} from "../handlers/evalTree";
+import _ from "lodash";
+import type {
+  WidgetEntityConfig,
+  WidgetEntity,
+} from "@appsmith/entities/DataTree/types";
+import { isWidget } from "@appsmith/workers/Evaluation/evaluationUtils";
+import { klona } from "klona";
+import { getDynamicBindings, isDynamicValue } from "utils/DynamicBindingUtils";
+import evaluateSync from "../evaluate";
+import type { DescendantWidgetMap } from "sagas/WidgetOperationUtils";
+import type { MetaState } from "reducers/entityReducers/metaReducer";
+import type { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidgetsReducer";
+import type { EvalMetaUpdates } from "@appsmith/workers/common/DataTreeEvaluator/types";
+import type { DataTree } from "entities/DataTree/dataTreeTypes";
+import { validateAndParseWidgetProperty } from "workers/common/DataTreeEvaluator/validationUtils";
+
+function resetWidgetFnDescriptor(
+  widgetName: string,
+  resetChildren = true,
+  metaUpdates: EvalMetaUpdates,
+) {
   return {
     type: "RESET_WIDGET_META_RECURSIVE_BY_NAME" as const,
-    payload: { widgetName, resetChildren },
+    payload: { widgetName, resetChildren, metaUpdates },
   };
 }
 
-export type TResetWidgetArgs = Parameters<typeof resetWidgetFnDescriptor>;
 export type TResetWidgetDescription = ReturnType<
   typeof resetWidgetFnDescriptor
 >;
 export type TResetWidgetActionType = TResetWidgetDescription["type"];
 
 async function resetWidget(
-  ...args: Parameters<typeof resetWidgetFnDescriptor>
+  ...args: [widgetName: string, resetChildren: boolean]
 ) {
-  return promisify(resetWidgetFnDescriptor)(...args);
+  const widgetName = args[0];
+  const resetChildren = args[1] || true;
+  const metaUpdates: EvalMetaUpdates = [];
+  const updatedProperties: string[][] = [];
+
+  resetWidgetMetaProperty(
+    widgetName,
+    resetChildren,
+    metaUpdates,
+    updatedProperties,
+  );
+
+  return promisify(resetWidgetFnDescriptor)(
+    widgetName,
+    resetChildren,
+    metaUpdates,
+  );
+}
+
+function resetWidgetMetaProperty(
+  widgetName: string,
+  resetChildren = true,
+  evalMetaUpdates: EvalMetaUpdates,
+  updatedProperties: string[][],
+) {
+  const widget: FlattenedWidgetProps | undefined = _.find(
+    Object.values(canvasWidgets || {}),
+    (widget) => widget.widgetName === widgetName,
+  );
+
+  if (!dataTreeEvaluator || !widget) return;
+
+  const evalTree = dataTreeEvaluator.getEvalTree();
+  const oldUnEvalTree = dataTreeEvaluator.getOldUnevalTree();
+  const configTree = dataTreeEvaluator.getConfigTree();
+  const evalProps = dataTreeEvaluator.getEvalProps();
+  const evalPathsIdenticalToState =
+    dataTreeEvaluator.getEvalPathsIdenticalToState();
+
+  const evaluatedEntity = evalTree[widget.widgetName];
+  const evaluatedEntityConfig = configTree[
+    widget.widgetName
+  ] as WidgetEntityConfig;
+
+  if (evaluatedEntity && isWidget(evaluatedEntity)) {
+    const metaObj = evaluatedEntity.meta;
+    const currentMetaProperties = Object.keys(metaObj);
+    const { propertyOverrideDependency } = evaluatedEntityConfig;
+
+    for (const propertyPath of currentMetaProperties) {
+      const defaultPropertyPath =
+        propertyOverrideDependency[propertyPath]?.DEFAULT;
+      if (defaultPropertyPath) {
+        const unEvalEntity = oldUnEvalTree[widget.widgetName] as WidgetEntity;
+        const expressionToEvaluate: string = unEvalEntity[defaultPropertyPath];
+
+        let finalValue: unknown;
+        if (
+          expressionToEvaluate &&
+          typeof expressionToEvaluate === "string" &&
+          isDynamicValue(expressionToEvaluate)
+        ) {
+          const { jsSnippets } = getDynamicBindings(expressionToEvaluate);
+          const { result } = evaluateSync(
+            jsSnippets[0] || expressionToEvaluate,
+            evalTree,
+            false,
+            {},
+            undefined,
+            configTree,
+          );
+
+          finalValue = klona(result);
+        } else {
+          finalValue = klona(expressionToEvaluate);
+        }
+
+        const parsedValue = validateAndParseWidgetProperty({
+          fullPropertyPath: `${widget.widgetName}.${defaultPropertyPath}`,
+          widget: unEvalEntity,
+          configTree,
+          evalPropertyValue: finalValue,
+          unEvalPropertyValue: expressionToEvaluate,
+          evalProps,
+          evalPathsIdenticalToState,
+        });
+
+        evalMetaUpdates.push({
+          widgetId: evaluatedEntity.widgetId,
+          metaPropertyPath: propertyPath.split("."),
+          value: parsedValue,
+        });
+
+        continue;
+      } else {
+        evalMetaUpdates.push({
+          widgetId: evaluatedEntity.widgetId,
+          metaPropertyPath: propertyPath.split("."),
+          value: undefined,
+        });
+      }
+    }
+
+    if (resetChildren) {
+      resetChildrenMetaProperty(
+        widget.widgetId,
+        evalTree,
+        evalMetaUpdates,
+        updatedProperties,
+      );
+    }
+  }
+}
+
+function sortWidgetsMetaByParent(widgetsMeta: MetaState, parentId: string) {
+  return _.reduce(
+    widgetsMeta,
+    function (
+      result: {
+        childrenWidgetsMeta: MetaState;
+        otherWidgetsMeta: MetaState;
+      },
+      currentWidgetMeta,
+      key,
+    ) {
+      return key.startsWith(parentId + "_")
+        ? {
+            ...result,
+            childrenWidgetsMeta: {
+              ...result.childrenWidgetsMeta,
+              [key]: currentWidgetMeta,
+            },
+          }
+        : {
+            ...result,
+            otherWidgetsMeta: {
+              ...result.otherWidgetsMeta,
+              [key]: currentWidgetMeta,
+            },
+          };
+    },
+    {
+      childrenWidgetsMeta: {},
+      otherWidgetsMeta: {},
+    },
+  );
+}
+
+export function getWidgetDescendantToReset(
+  canvasWidgets: CanvasWidgetsReduxState,
+  widgetId: string,
+  evaluatedDataTree: DataTree,
+  widgetsMeta: MetaState,
+): DescendantWidgetMap[] {
+  const descendantList: DescendantWidgetMap[] = [];
+  const widget = _.get(canvasWidgets, widgetId);
+
+  const sortedWidgetsMeta = sortWidgetsMetaByParent(widgetsMeta, widgetId);
+  for (const childMetaWidgetId of Object.keys(
+    sortedWidgetsMeta.childrenWidgetsMeta,
+  )) {
+    const evaluatedChildWidget = _.find(evaluatedDataTree, function (entity) {
+      return isWidget(entity) && entity.widgetId === childMetaWidgetId;
+    }) as WidgetEntity | undefined;
+    descendantList.push({
+      id: childMetaWidgetId,
+      evaluatedWidget: evaluatedChildWidget,
+    });
+    const grandChildren = getWidgetDescendantToReset(
+      canvasWidgets,
+      childMetaWidgetId,
+      evaluatedDataTree,
+      sortedWidgetsMeta.otherWidgetsMeta,
+    );
+    if (grandChildren.length) {
+      descendantList.push(...grandChildren);
+    }
+  }
+
+  if (widget) {
+    const { children = [] } = widget;
+
+    if (children && children.length) {
+      for (const childIndex in children) {
+        if (children.hasOwnProperty(childIndex)) {
+          const childWidgetId = children[childIndex];
+
+          const childCanvasWidget = _.get(canvasWidgets, childWidgetId);
+          const childWidgetName = childCanvasWidget.widgetName;
+          const childWidget = evaluatedDataTree[childWidgetName];
+          if (isWidget(childWidget)) {
+            descendantList.push({
+              id: childWidgetId,
+              evaluatedWidget: childWidget,
+            });
+            const grandChildren = getWidgetDescendantToReset(
+              canvasWidgets,
+              childWidgetId,
+              evaluatedDataTree,
+              sortedWidgetsMeta.otherWidgetsMeta,
+            );
+            if (grandChildren.length) {
+              descendantList.push(...grandChildren);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return descendantList;
+}
+
+function resetChildrenMetaProperty(
+  parentWidgetId: string,
+  evaluatedDataTree: DataTree,
+  evalMetaUpdates: EvalMetaUpdates,
+  updatedProperties: string[][],
+) {
+  const childrenList = getWidgetDescendantToReset(
+    canvasWidgets,
+    parentWidgetId,
+    evaluatedDataTree,
+    canvasWidgetsMeta,
+  );
+
+  for (const childIndex in childrenList) {
+    const { evaluatedWidget: childWidget } = childrenList[childIndex];
+
+    if (!childWidget) continue;
+
+    resetWidgetMetaProperty(
+      childWidget?.widgetName,
+      false,
+      evalMetaUpdates,
+      updatedProperties,
+    );
+  }
 }
 
 export default resetWidget;

app/client/src/workers/Evaluation/handlers/evalTree.ts
@@ -27,9 +27,13 @@ import DataStore from "../dataStore";
 import type { TransmissionErrorHandler } from "../fns/utils/Messenger";
 import { MessageType, sendMessage } from "utils/MessageUtil";
 import { startSpansInAnEvaluation } from "UITelemetry/generateWebWorkerTraces";
+import type { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidgetsReducer";
+
 export let replayMap: Record<string, ReplayEntity<any>> | undefined;
 export let dataTreeEvaluator: DataTreeEvaluator | undefined;
 export const CANVAS = "canvas";
+export let canvasWidgetsMeta: Record<string, any>;
+export let canvasWidgets: CanvasWidgetsReduxState;
 
 export default function (request: EvalWorkerSyncRequest) {
   const { data } = request;
@@ -56,11 +60,15 @@ export default function (request: EvalWorkerSyncRequest) {
     theme,
     unevalTree: __unevalTree__,
     widgets,
+    widgetsMeta,
     widgetTypeConfigMap,
   } = data as EvalTreeRequestData;
 
   const unevalTree = __unevalTree__.unEvalTree;
   configTree = __unevalTree__.configTree as ConfigTree;
+  canvasWidgets = widgets;
+  canvasWidgetsMeta = widgetsMeta;
+
   try {
     if (!dataTreeEvaluator) {
       isCreateFirstTree = true;

app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug27119_Spec.ts
@@ -1,0 +1,83 @@
+import {
+  entityExplorer,
+  propPane,
+  draggableWidgets,
+  agHelper,
+  deployMode,
+  locators,
+  jsEditor,
+} from "../../../../support/Objects/ObjectsCore";
+
+describe("Reset widget action", () => {
+  it("Reset widget to default after setValue has been applied", () => {
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.INPUT_V2);
+    propPane.UpdatePropertyFieldValue("Default value", "John");
+
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON, 300, 300);
+    propPane.EnterJSContext("onClick", `{{Input1.setValue('Hello!')}}`);
+    propPane.UpdatePropertyFieldValue("Label", "Set value");
+
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON, 500, 100);
+    propPane.EnterJSContext("onClick", `{{resetWidget("Input1")}}`);
+    propPane.UpdatePropertyFieldValue("Label", "Reset value");
+
+    deployMode.DeployApp();
+
+    agHelper.ClickButton("Set value");
+    agHelper.AssertText(
+      locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+      "val",
+      "Hello!",
+    );
+
+    agHelper.ClickButton("Reset value");
+    agHelper.AssertText(
+      locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+      "val",
+      "John",
+    );
+
+    agHelper.ClearNType(
+      locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+      "Testing",
+    );
+    agHelper.ClickButton("Reset value");
+    agHelper.AssertText(
+      locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+      "val",
+      "John",
+    );
+  });
+  it("Reset value is accessible after 'awaiting'", () => {
+    deployMode.NavigateBacktoEditor();
+    agHelper.ClearNType(locators._input, "Meta Text");
+
+    const JS_OBJECT_BODY = `export default {
+      async resetInputWithoutAwait () {
+         resetWidget('Input1')
+         showAlert(Input1.text)	
+      },
+      async resetInputWithAwait () {
+      await resetWidget('Input1')
+      showAlert(Input1.text)	
+      }
+    }`;
+
+    // Create js object
+    jsEditor.CreateJSObject(JS_OBJECT_BODY, {
+      paste: true,
+      completeReplace: true,
+      toRun: false,
+      prettify: false,
+      shouldCreateNewJSObj: true,
+    });
+    agHelper.Sleep(4000);
+    jsEditor.SelectFunctionDropdown("resetInputWithoutAwait");
+    agHelper.ClickButton("Run");
+    agHelper.AssertContains("Meta Text");
+
+    jsEditor.SelectFunctionDropdown("resetInputWithAwait");
+    agHelper.ClickButton("Run");
+    agHelper.AssertContains("John");
+  });
+});

app/client/src/actions/metaActions.ts
@@ -55,6 +55,18 @@ export const resetWidgetMetaProperty = (
   });
 };
 
+export const resetWidgetMetaUpdates = (
+  evalMetaUpdates: EvalMetaUpdates,
+): BatchAction<ResetWidgetMetaPayload> => {
+  return batchAction({
+    type: ReduxActionTypes.RESET_WIDGET_META_UPDATES,
+    payload: {
+      evalMetaUpdates,
+    },
+    postEvalActions: [{ type: ReduxActionTypes.RESET_WIDGET_META_EVALUATED }],
+  });
+};
+
 export const resetChildrenMetaProperty = (
   widgetId: string,
 ): ReduxAction<{ widgetId: string }> => {

commit 7f658b264030d68a8c7148efce0bd74afbdee904

fix: resetWidget doesn't reset child widgets inside a list (#30121)
## Description
> Fixes issue with resetWidget platform function not honouring
`resetChildren` argument. Also, takes care of resetting meta widgets
inside a list.
These bug was introduced in #29151, where we moved the resetWidget logic
to the worker thread, to support restoration of default values in meta
fields (post setter methods invocation).
>
>
#### PR fixes following issue(s)
Fixes #30116 

#### Type of change
- Bug fix (non-breaking change which fixes an issue)
>
## Testing
>
#### How Has This Been Tested?
- [x] Manual
>
#### Test Plan
> Add Testsmith test cases links that relate to this PR
>
>
#### Issues raised during DP testing
> Link issues raised during DP testing for better visiblity and tracking
(copy link from comments dropped on this PR)
>
>
>
## Checklist:
#### Dev activity
- [x] My code follows the style guidelines of this project
- [x] I have performed a self-review of my own code
- [ ] I have commented my code, particularly in hard-to-understand areas
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [x] New and existing unit tests pass locally with my changes
- [ ] PR is being merged under a feature flag


#### QA activity:
- [ ] [Speedbreak
features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
have been covered
- [ ] Test plan covers all impacted features and [areas of
interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
- [ ] Test plan has been peer reviewed by project stakeholders and other
QA members
- [ ] Manually tested functionality on DP
- [ ] We had an implementation alignment call with stakeholders post QA
Round 2
- [ ] Cypress test cases have been added and approved by SDET/manual QA

app/client/src/sagas/EvaluationsSaga.ts
@@ -254,8 +254,7 @@ export function* evaluateTreeSaga(
     yield select(getMetaWidgets);
   const theme: ReturnType<typeof getSelectedAppTheme> =
     yield select(getSelectedAppTheme);
-  const toPrintConfigTree = unEvalAndConfigTree.configTree;
-  log.debug({ unevalTree, configTree: toPrintConfigTree });
+  log.debug({ unevalTree, configTree: unEvalAndConfigTree.configTree });
   PerformanceTracker.startAsyncTracking(
     PerformanceTransactionName.DATA_TREE_EVALUATION,
   );

app/client/src/workers/Evaluation/fns/resetWidget.ts
@@ -5,6 +5,7 @@ import {
   canvasWidgets,
   dataTreeEvaluator,
   canvasWidgetsMeta,
+  metaWidgetsCache,
 } from "../handlers/evalTree";
 import _ from "lodash";
 import type {
@@ -42,7 +43,7 @@ async function resetWidget(
   ...args: [widgetName: string, resetChildren: boolean]
 ) {
   const widgetName = args[0];
-  const resetChildren = args[1] || true;
+  const resetChildren = args[1];
   const metaUpdates: EvalMetaUpdates = [];
   const updatedProperties: string[][] = [];
 
@@ -66,12 +67,21 @@ function resetWidgetMetaProperty(
   evalMetaUpdates: EvalMetaUpdates,
   updatedProperties: string[][],
 ) {
-  const widget: FlattenedWidgetProps | undefined = _.find(
+  if (!dataTreeEvaluator) return;
+
+  let widget: FlattenedWidgetProps | undefined = _.find(
     Object.values(canvasWidgets || {}),
     (widget) => widget.widgetName === widgetName,
   );
 
-  if (!dataTreeEvaluator || !widget) return;
+  if (!widget) {
+    widget = _.find(
+      Object.values(metaWidgetsCache || {}),
+      (widget) => widget.widgetName === widgetName,
+    );
+  }
+
+  if (!widget) return;
 
   const evalTree = dataTreeEvaluator.getEvalTree();
   const oldUnEvalTree = dataTreeEvaluator.getOldUnevalTree();
@@ -129,7 +139,9 @@ function resetWidgetMetaProperty(
         });
 
         evalMetaUpdates.push({
-          widgetId: evaluatedEntity.widgetId,
+          widgetId: evaluatedEntity.isMetaWidget
+            ? (evaluatedEntity.metaWidgetId as string)
+            : evaluatedEntity.widgetId,
           metaPropertyPath: propertyPath.split("."),
           value: parsedValue,
         });
@@ -137,7 +149,9 @@ function resetWidgetMetaProperty(
         continue;
       } else {
         evalMetaUpdates.push({
-          widgetId: evaluatedEntity.widgetId,
+          widgetId: evaluatedEntity.isMetaWidget
+            ? (evaluatedEntity.metaWidgetId as string)
+            : evaluatedEntity.widgetId,
           metaPropertyPath: propertyPath.split("."),
           value: undefined,
         });
@@ -250,7 +264,6 @@ export function getWidgetDescendantToReset(
       }
     }
   }
-
   return descendantList;
 }
 

app/client/src/workers/Evaluation/handlers/evalTree.ts
@@ -28,11 +28,13 @@ import type { TransmissionErrorHandler } from "../fns/utils/Messenger";
 import { MessageType, sendMessage } from "utils/MessageUtil";
 import { startSpansInAnEvaluation } from "UITelemetry/generateWebWorkerTraces";
 import type { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidgetsReducer";
+import type { MetaWidgetsReduxState } from "reducers/entityReducers/metaWidgetsReducer";
 
 export let replayMap: Record<string, ReplayEntity<any>> | undefined;
 export let dataTreeEvaluator: DataTreeEvaluator | undefined;
 export const CANVAS = "canvas";
 export let canvasWidgetsMeta: Record<string, any>;
+export let metaWidgetsCache: MetaWidgetsReduxState;
 export let canvasWidgets: CanvasWidgetsReduxState;
 
 export default function (request: EvalWorkerSyncRequest) {
@@ -68,6 +70,7 @@ export default function (request: EvalWorkerSyncRequest) {
   configTree = __unevalTree__.configTree as ConfigTree;
   canvasWidgets = widgets;
   canvasWidgetsMeta = widgetsMeta;
+  metaWidgetsCache = metaWidgets;
 
   try {
     if (!dataTreeEvaluator) {

app/client/src/workers/Evaluation/handlers/jsLibrary.ts
@@ -108,23 +108,45 @@ export async function installLibrary(
 
     let module = null;
     try {
+      /**
+       * Try to import the library using importScripts
+       * This works for UMD modules
+       * If this fails, we try to import the library using dynamic import
+       */
       self.importScripts(url);
+
       // Find keys add that were installed to the global scope.
       const keysAfterInstallation = Object.keys(self);
       accessors.push(
         ...difference(keysAfterInstallation, envKeysBeforeInstallation),
       );
 
-      // Check the list of installed library to see if their values have changed.
-      // This is to check if the newly installed library overwrites an already existing
+      /**
+       * Check the list of installed library to see if their values have changed.
+       * This is to check if the newly installed library overwrites an already existing one
+       * For eg. let's say lodash v1 and v2 both have the same accessor `_`.
+       * If lodash v1 is installed first, `_` will be added to the global scope.
+       * This value is stored in libStore. ie libStore['_'] = lodash v1
+       * Now when lodash v2 is installed, `_` will be added to the global scope again.
+       * We check if the value of `_` has changed to detect this. ie . libStore['_'] !== lodash v2
+       * If it has changed, we add the new value to the global scope with a unique name.
+       * */
       accessors.push(
         ...Object.keys(libStore).filter((k) => {
           return libStore[k] !== self[k];
         }),
       );
 
+      /**
+       * Certain libraries are added to the global scope with { configurable: false }
+       * These libraries cannot be removed from the global scope via delete operation so we set them to undefined them during uninstall request.
+       * These library accessors are removed from takenAccessors list.
+       * So they will remain in the global scope even after uninstallation. self[accessor] = undefined.
+       * Any attempt to reinstall the same library will overwrite the library with undefined and we need to detect these accessors.
+       */
       accessors.push(...unsetLibraryKeys.filter((k) => self[k] !== undefined));
 
+      // Generate unique names for accessors that are already taken.
       for (let i = 0; i < accessors.length; i++) {
         if (
           takenNamesMap.hasOwnProperty(accessors[i]) ||
@@ -142,7 +164,10 @@ export async function installLibrary(
     } catch (e) {
       log.debug(e, `importScripts failed for ${url}`);
       try {
+        // If importScripts fails, try to import the library using dynamic import
         module = await import(/* webpackIgnore: true */ url);
+
+        // If the module is not an object, it is not a valid ESM library
         if (module && typeof module === "object") {
           const uniqAccessor = generateUniqueAccessor(
             url,
@@ -163,6 +188,7 @@ export async function installLibrary(
       throw new Error("Unable to determine a unique accessor");
     }
 
+    // Name of the library is the last accessor. This is totally random and needs fixing.
     const name = accessors[accessors.length - 1];
 
     defs["!name"] = `LIB/${name}`;
@@ -180,7 +206,8 @@ export async function installLibrary(
       );
     }
 
-    // All the existing library and the newly installed one is added to global.
+    // Restore the libraries from libStore to the global scope.
+    // This is done to ensure that the libraries are not overwritten by the newly installed library.
     Object.keys(libStore).forEach((k) => (self[k] = libStore[k]));
 
     //Reserve accessor names.
@@ -246,7 +273,8 @@ export async function loadLibraries(
           ...Object.keys(libStore).filter((k) => libStore[k] !== self[k]),
         );
 
-        /** Sort the accessor list from backend and installed accessor list using the same rule to apply all modifications.
+        /**
+         * Sort the accessor list from backend and installed accessor list using the same rule to apply all modifications.
          * This is required only for UMD builds, since we always generate unique names for ESM.
          */
         accessors.sort();
@@ -303,8 +331,12 @@ function generateUniqueAccessor(
   try {
     // Checks to see if a URL was passed
     const urlObject = new URL(urlOrName);
-    // URL pattern for ESM modules from jsDelivr - https://cdn.jsdelivr.net/npm/stripe@13.3.0/+esm
-    // Assuming the file name is the last part of the path
+    /**
+     * URL pattern for ESM modules from jsDelivr - https://cdn.jsdelivr.net/npm/stripe@13.3.0/+esm
+     * Assuming the file name is the last part of the path
+     * TODO: Find a better way to extract the file name from the URL
+     * TODO: Handle the case where the URL is from a different CDN like unpkg, cdnjs etc.
+     */
     const urlPathParts = urlObject.pathname.split("/");
     name = urlPathParts.pop() as string;
     name = name?.includes("+esm") ? (urlPathParts.pop() as string) : name;
@@ -319,6 +351,11 @@ function generateUniqueAccessor(
     return validVar;
   }
   let index = 0;
+  /**
+   * If the accessor is already taken, generate a unique name by appending an index to the accessor.
+   * The index is incremented until a unique name is found.
+   * 100 is a very large number and this loop should never run more than a few times.
+   */
   while (index++ < 100) {
     const name = `${validVar}_${index}`;
     if (!takenAccessors.includes(name) && !takenNamesMap.hasOwnProperty(name)) {

app/client/cypress/e2e/Regression/ServerSide/JsFunctionExecution/PlatformFn_spec.ts
@@ -1,4 +1,3 @@
-import { data } from "cypress/types/jquery";
 import {
   agHelper,
   apiPage,
@@ -6,7 +5,9 @@ import {
   debuggerHelper,
   dataManager,
   locators,
+  draggableWidgets,
 } from "../../../../support/Objects/ObjectsCore";
+import EditorNavigation from "../../../../support/Pages/EditorNavigation";
 
 describe(
   "Tests functionality of platform function",
@@ -176,6 +177,53 @@ describe(
       agHelper.AssertElementLength(locators._toastMsg, 2);
       agHelper.ValidateToastMessage("Hello World", 0);
       agHelper.ValidateToastMessage("Hello World", 1);
+    });
+
+    it("3. Bug 30121 Reset widget should reset children as well when resetChildren argument is set to true", () => {
+      EditorNavigation.ShowCanvas();
+      agHelper.AddDsl("resetWidgetDSL");
+      agHelper.ClearNType(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+        "Hello! How are you?",
+        0,
+      );
+
+      agHelper.ClickButton("ResetContainer");
+
+      agHelper.AssertText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+        "text",
+        "",
+        0,
+      );
+
+      agHelper.ClearNType(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+        "Hello! How are you?",
+        1,
+      );
+
+      agHelper.ClearNType(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+        "Hello! How are you?",
+        2,
+      );
+
+      agHelper.ClickButton("ResetList");
+
+      agHelper.AssertText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+        "text",
+        "",
+        1,
+      );
+
+      agHelper.AssertText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) + " input",
+        "text",
+        "",
+        2,
+      );
     });
   },
 );